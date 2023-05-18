5d516035a0ad34cf38dd256300c0a1e1fcc0a2e7
Valeria Khristinina <valeria.khristinina@gmail.com>
2023-05-18 16:28:38 +0200

    added some components on index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import { type AppType } from "next/app";
-
+import { type AppType, type AppProps } from "next/app";
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps }:AppProps) => {
+  return   <>
+  <Head>
+    <title>Page title</title>
+    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+  </Head>
+
+  <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+      /** Put your mantine theme override here */
+      colorScheme: 'light',
+    }}
+  >
+    <Component {...pageProps} />
+  </MantineProvider>
+</>;
 };
 
 export default api.withTRPC(MyApp);
