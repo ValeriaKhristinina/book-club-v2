9ba807812f8808738f575ca9b76c1147aef972ea
Valeria Khristinina <valeria.khristinina@gmail.com>
2023-06-07 22:36:57 +0200

    create layout component

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,43 @@
+import styles from './layout.module.css';
+import { Button, Container, Group, Title, Text } from '@mantine/core';
+import Link from 'next/link';
+
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <Container className={styles.container}>
+      <header className={styles.header}>
+        <Group position="apart" w="100%">
+          <Group>
+            <Title size="20px">
+              <Link href={'/'}>Book Club</Link>
+            </Title>
+          </Group>
+          <Group>
+            <Button>Login</Button>
+          </Group>
+        </Group>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        <Group position="right">
+          <Group position="left">
+            <Text>
+              Created by{' '}
+              <i>
+                <Link href="https://github.com/ValeriaKhristinina">
+                  Lera Khristinina
+                </Link>
+              </i>
+            </Text>
+          </Group>
+        </Group>
+      </footer>
+    </Container>
+  );
+}
+
+export default Layout;
