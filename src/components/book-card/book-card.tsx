3d8feef6b210ed9719b4706ac6d276fa708ee457
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-06-23 13:22:50 +0200

    Add loader on progress culculation

--- src/components/book-card/book-card.tsx
+++ src/components/book-card/book-card.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './book-card.module.css';
-import dayjs from 'dayjs';
-import { api } from '~/utils/api';
-import { type Meeting } from '~/types/meeting';
-import { Pencil } from 'tabler-icons-react';
-import Link from 'next/link'
+import React, { useState, useEffect } from "react";
+import styles from "./book-card.module.css";
+import dayjs from "dayjs";
+import { api } from "~/utils/api";
+import { type Meeting } from "~/types/meeting";
+import { Pencil } from "tabler-icons-react";
+import Link from "next/link";
 import {
   Card,
   Group,
@@ -15,16 +15,17 @@ import {
   Avatar,
   Container,
   Progress,
-  ActionIcon
-} from '@mantine/core';
+  ActionIcon,
+  Flex,
+  Loader
+} from "@mantine/core";
 import {
   calculateAverageRating,
   checkProgressColor,
   getRatedParticipants,
   getVisitedParticipants,
   getVisitingProgress
-} from '~/utils/utils';
-
+} from "~/utils/utils";
 
 interface BookCardProps {
   meeting: Meeting;
@@ -42,9 +43,7 @@ function BookCard({ meeting }: BookCardProps) {
     date: meeting.date
   });
 
-  const actualMembers = actualMembersQuery.data ? actualMembersQuery.data : []
-
-
+  const actualMembers = actualMembersQuery.data ? actualMembersQuery.data : [];
 
   const { data: choosedMember } = api.members.getById.useQuery({
     id: meeting.chosenById || 0
@@ -57,13 +56,20 @@ function BookCard({ meeting }: BookCardProps) {
     visitedParticipants.length,
     actualMembers.length
   );
+  let isLoadingProgress = true;
+
+  if (visitingProgress > 0 || !meeting.isComplete) {
+    isLoadingProgress = false;
+  }
+
+   const progress = (meeting.isComplete && !isLoadingProgress) ? visitingProgress : 0;
 
   return (
     <Card w="220px" shadow="xl" padding="12px" className={styles.bookCard}>
-      <Group position="apart" mb="12px">
+      <Flex justify={"space-between"} mb="12px">
         <Group>
           <Rating fractions={16} defaultValue={averageRating} readOnly />
-          <Text size="xs">({averageRating})</Text>
+          <Text size="xs">({averageRating ? averageRating : 0})</Text>
           <Group className={styles.changeMeeting}>
             <Link href={`/meetings/${meeting.id}`}>
               <ActionIcon>
@@ -75,40 +81,46 @@ function BookCard({ meeting }: BookCardProps) {
         <Text size="xs">
           {ratedParticipants.length}/{actualMembers.length}
         </Text>
-      </Group>
-      <Group position="center" mb="12px">
+      </Flex>
+      <Flex justify={"center"} mb="12px">
         <Image
           className={styles.center}
           src="/book-image.png"
           width="120px"
           alt="rando photo"
         />
-      </Group>
-      <Title size="md" align="center" mb="12px">
+      </Flex>
+      <Title size="md" mb="12px">
         {meeting.title} by {meeting.author}
       </Title>
-      <Group position="apart" mb="12px">
+      <Flex justify="space-between" mb="12px">
         <Avatar size="sm" radius="xl">
           {choosedMember?.firstName.charAt(0)}
           {choosedMember?.lastName.charAt(0)}
         </Avatar>
         <Text size="xs">{dayjs(meeting.date).format("D MMM YYYY")}</Text>
-      </Group>
+      </Flex>
 
       <Container p="0px">
+        {isLoadingProgress && (
+          <section>
+            <Loader color="cyan" className={styles.loader} size="xs" />
+          </section>
+        )}
+
         <Progress
-          value={visitingProgress}
-          label={`${visitingProgress}%`}
+          value={progress}
+          label={`${progress}%`}
           size="xl"
           radius="xl"
-          color={checkProgressColor(visitingProgress)}
+          color={checkProgressColor(progress)}
         />
-        <Group position="apart">
+        <Flex justify="space-between">
           <Text size="xs">
             {visitedParticipants.length}/{actualMembers.length}
           </Text>
-          <Text size="xs">{visitingProgress}%</Text>
-        </Group>
+          <Text size="xs">{progress}%</Text>
+        </Flex>
       </Container>
     </Card>
   );
