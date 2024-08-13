5bcb02977e69dcfdb47f926df6dbf266a9a90c3c
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-08-13 19:08:03 +0200

    add Vercel Cron

---
+++ src/server/api/cron.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Cron check");
+  console.log("req", req);
+  console.log("res", res);
+  res.status(200).end("Hello Cron!");
+}
