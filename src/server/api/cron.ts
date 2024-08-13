5ee11e26ead8a4405cd280e0bc4d8a7e13ad4cc7
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-08-13 19:23:10 +0200

    fix

--- src/server/api/cron.ts
+++ src/server/api/cron.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Cron check");
-  console.log("req", req);
+
   console.log("res", res);
   res.status(200).end("Hello Cron!");
 }
