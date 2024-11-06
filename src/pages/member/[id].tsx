c7618ee3f2646a12ea1549b84c2ac66367aafd7c
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-11-06 13:24:42 +0100

    work on member page

--- src/pages/member/[id].tsx
+++ src/pages/member/[id].tsx
@@ -1,30 +1,105 @@
-import { type NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { api } from '~/utils/api';
+import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
 
-import Layout from '~/components/layout/layout';
+import Layout from "~/components/layout/layout";
+import { Button, List } from "@mantine/core";
+import { useState } from "react";
 
 const Member: NextPage = () => {
   const router = useRouter();
   const memberID = router.query.id;
+  const [isShowAll, setIsShowAll] = useState(false);
 
   const { data: member } = api.members.getById.useQuery({
     id: Number(memberID)
   });
-  const visetedMiitings = member?.meetings.filter(
-    (meeting) => meeting.isVisited
-  );
+  const visetedMeetings = member?.meetings
+    .filter((meeting) => meeting.isVisited)
+    .reverse();
+
+  const showMeeting = 5;
 
   return (
     <Layout>
       <>
-        <h1>Hello! {member?.firstName}</h1>
+        <h1>
+          {member?.firstName} {member?.lastName}
+        </h1>
 
-        <ul>
-          {visetedMiitings?.map((meeting) => {
-            return <li key={meeting.meetingId}>{meeting.meetingId}</li>;
+        <h2>Last {showMeeting} visited meetings</h2>
+        <List className="mb-4">
+          {visetedMeetings?.slice(0, showMeeting).map((meeting) => {
+            return (
+              <List.Item
+                key={meeting.meetingId}
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                    <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                    <path d="M3 6l0 13" />
+                    <path d="M12 6l0 13" />
+                    <path d="M21 6l0 13" />
+                  </svg>
+                }
+              >
+                {meeting.meeting.title} by {meeting.meeting.author}
+              </List.Item>
+            );
           })}
-        </ul>
+        </List>
+        {isShowAll && (
+          <List className="mb-4">
+            {visetedMeetings?.slice(showMeeting).map((meeting) => {
+              return (
+                <List.Item
+                  key={meeting.meetingId}
+                  icon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                      <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                      <path d="M3 6l0 13" />
+                      <path d="M12 6l0 13" />
+                      <path d="M21 6l0 13" />
+                    </svg>
+                  }
+                >
+                  {meeting.meeting.title} by {meeting.meeting.author}
+                </List.Item>
+              );
+            })}
+          </List>
+        )}
+        <Button
+          variant="light"
+          color="cyan"
+          onClick={() => setIsShowAll(!isShowAll)}
+          className="mt-4"
+        >
+          {isShowAll ? "Hide visited meeting" : "Show all visited meetings"}
+        </Button>
       </>
     </Layout>
   );
