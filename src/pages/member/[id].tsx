086c06f65b773cf96bae89d9c9f9c17038da83ca
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-11-06 13:54:50 +0100

    work on member page

--- src/pages/member/[id].tsx
+++ src/pages/member/[id].tsx
@@ -3,13 +3,37 @@ import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 
 import Layout from "~/components/layout/layout";
-import { Button, List } from "@mantine/core";
+import { Box, Button, Flex, List } from "@mantine/core";
 import { useState } from "react";
+
+const BookIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+      <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+      <path d="M3 6l0 13" />
+      <path d="M12 6l0 13" />
+      <path d="M21 6l0 13" />
+    </svg>
+  );
+};
 
 const Member: NextPage = () => {
   const router = useRouter();
   const memberID = router.query.id;
-  const [isShowAll, setIsShowAll] = useState(false);
+  const [isShowAllVisited, setIsShowAllVisited] = useState(false);
+  const [isShowAllChoosen, setIsShowAllChoosen] = useState(false);
 
   const { data: member } = api.members.getById.useQuery({
     id: Number(memberID)
@@ -27,79 +51,70 @@ const Member: NextPage = () => {
           {member?.firstName} {member?.lastName}
         </h1>
 
-        <h2>Last {showMeeting} visited meetings</h2>
-        <List className="mb-4">
-          {visetedMeetings?.slice(0, showMeeting).map((meeting) => {
-            return (
-              <List.Item
-                key={meeting.meetingId}
-                icon={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                    <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                    <path d="M3 6l0 13" />
-                    <path d="M12 6l0 13" />
-                    <path d="M21 6l0 13" />
-                  </svg>
-                }
-              >
-                {meeting.meeting.title} by {meeting.meeting.author}
-              </List.Item>
-            );
-          })}
-        </List>
-        {isShowAll && (
-          <List className="mb-4">
-            {visetedMeetings?.slice(showMeeting).map((meeting) => {
-              return (
-                <List.Item
-                  key={meeting.meetingId}
-                  icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                      <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                      <path d="M3 6l0 13" />
-                      <path d="M12 6l0 13" />
-                      <path d="M21 6l0 13" />
-                    </svg>
-                  }
-                >
-                  {meeting.meeting.title} by {meeting.meeting.author}
+        <Flex direction="row" justify={"space-between"}>
+          <Box w="50%" mr="24px">
+            <h2>Last {showMeeting} visited meetings</h2>
+            <List className="mb-4">
+              {visetedMeetings?.slice(0, showMeeting).map((meeting) => {
+                return (
+                  <List.Item key={meeting.meetingId} icon={<BookIcon />}>
+                    {meeting.meeting.title} by {meeting.meeting.author}
+                  </List.Item>
+                );
+              })}
+            </List>
+            {isShowAllVisited && (
+              <List className="mb-4">
+                {visetedMeetings?.slice(showMeeting).map((meeting) => {
+                  return (
+                    <List.Item key={meeting.meetingId} icon={<BookIcon />}>
+                      {meeting.meeting.title} by {meeting.meeting.author}
+                    </List.Item>
+                  );
+                })}
+              </List>
+            )}
+            <Button
+              variant="light"
+              color="cyan"
+              onClick={() => setIsShowAllVisited(!isShowAllVisited)}
+            >
+              {isShowAllVisited
+                ? "Hide visited meeting"
+                : "Show all visited meetings"}
+            </Button>
+          </Box>
+          <Box w="50%">
+            <h2>Choosen book</h2>
+            <List>
+              {member?.chosenMeetings.slice(0, showMeeting).map((book) => (
+                <List.Item key={book.id} icon={<BookIcon />}>
+                  {book.title}
                 </List.Item>
-              );
-            })}
-          </List>
-        )}
-        <Button
-          variant="light"
-          color="cyan"
-          onClick={() => setIsShowAll(!isShowAll)}
-          className="mt-4"
-        >
-          {isShowAll ? "Hide visited meeting" : "Show all visited meetings"}
-        </Button>
+              ))}
+            </List>
+            {isShowAllChoosen && (
+              <List className="mb-4">
+                {member?.chosenMeetings?.slice(showMeeting).map((book) => {
+                  return (
+                    <List.Item key={book.id} icon={<BookIcon />}>
+                      {book.title} by {book.author}
+                    </List.Item>
+                  );
+                })}
+              </List>
+            )}
+            <Button
+              variant="light"
+              color="cyan"
+              onClick={() => setIsShowAllChoosen(!isShowAllChoosen)}
+            >
+              {isShowAllVisited
+                ? "Hide choosen meeting"
+                : "Show all choosen meetings"}
+            </Button>
+          </Box>
+        </Flex>
       </>
     </Layout>
   );
